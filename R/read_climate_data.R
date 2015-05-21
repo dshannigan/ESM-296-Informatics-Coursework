f9521b379542ca6dc52ae9f5e954f3ff3d0965f6
Dan Hannigan <dhannigan@bren.ucsb.edu>
2015-05-20 18:04:23 -0700

    commented

--- R/read_climate_data.R
+++ R/read_climate_data.R
@@ -1,3 +1,7 @@
+###' @title Function for reading in files
+###' @param directory. absolute file path of target input file surrounded by quotes (e.x. 'C:/cygwin64/home/Dan/esm296/ESM-296-Informatics-Coursework/clim.txt').
+###' @return Writes the input file to the RStudio global environment for use with other functions. 
+
 read_climate = function(directory){
 
   test = read.table(directory, header = T)
